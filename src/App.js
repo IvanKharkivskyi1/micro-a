ec7287ae4af8d12a05b4c5a8cfe25e3f3bc2c5aa
IvanKharkivskyi1 <ikcomua@gmail.com>
2024-10-17 18:25:58 +0300

    docker

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Microfrontend AAA AA</div>;
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,37 @@
-new ModuleFederationPlugin({
-    name: 'microA',
-    filename: 'remoteEntry.js',
-    exposes: {
-      './App': './src/App',
-    },
-  });
-  
+const { ModuleFederationPlugin } = require("webpack").container;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "production",
+  output: {
+    publicPath: "auto",
+  },
+  devServer: {
+    port: 3001,
+    hot: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "microA",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App",
+      },
+      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
